44ef65883fc54919011f90d1c247054874eafaab
Yokohide0317 <yokohide0317@gmail.com>
2024-05-28 18:51:18 +0900

    2024年用に変更

--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -3,7 +3,7 @@
 set -eux
 
 docker run -it \
-  -p 2024:8888 \
+  -p 8080:8888 \
   -v $PWD/notebooks/user01:/home/user01/notebook \
   -v $PWD/notebooks/user02:/home/user02/notebook \
   -v $PWD/notebooks/user03:/home/user03/notebook \
@@ -14,5 +14,7 @@ docker run -it \
   -v $PWD/notebooks/user08:/home/user08/notebook \
   -v $PWD/notebooks/user09:/home/user09/notebook \
   -v $PWD/notebooks/user10:/home/user10/notebook \
+  -v $PWD/notebooks/user11:/home/user11/notebook \
+  -v $PWD/notebooks/user12:/home/user12/notebook \
   --rm \
   qiime2-hub
